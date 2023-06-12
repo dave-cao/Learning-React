67ed68024c6be3774c5c67865abc2dc57d08508b
David Cao <sirdavidcao@gmail.com>
2023-06-11 22:58:31 -0600

    started next.js tutorial

---
+++ react-blog/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function firstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
